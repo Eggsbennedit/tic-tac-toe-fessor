b6e917121602c2901a44a16849cac6ae0de0aa19
Marcos <112193426+Eggsbennedit@users.noreply.github.com>
2024-04-26 20:33:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
 var jogador = "x";
-const quadrados = document.getElementsByClassName['quadrado']
-
-var j1 = []
-var j2 = []
 
 function jogar(celula){
     if(celula.innerHTML == ""){
         celula.innerHTML = jogador;
-        for(i in quadrados){
-            if(i == celula){
-                j1.append(i)
-            }
-        }
+
         //console.log(celula)
         if(jogador == "x"){
             jogador = "o";
